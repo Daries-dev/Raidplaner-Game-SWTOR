c7fa9a51b9fa733dd7135341326fa9518e87d4ce
xDeraiser <deraiser@gmx.de>
2024-04-29 12:43:36 +0200

    Applied CC BY-SA 4.0 license to the project

--- files/lib/system/event/listener/SWTORCharacterAddCreateFormListener.class.php
+++ files/lib/system/event/listener/SWTORCharacterAddCreateFormListener.class.php
@@ -25,7 +25,7 @@ use wcf\system\form\builder\IFormDocument;
  * 
  * @author  Marco Daries
  * @copyright   2023-2024 Daries.dev
- * @license Free License <https://daries.dev/en/license-for-free-plugins>
+ * @license Raidplaner is licensed under Creative Commons Attribution-ShareAlike 4.0 International
  */
 final class SWTORCharacterAddCreateFormListener
 {
